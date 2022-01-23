ccae16582e41205a30efd5d50bc521c84f828e63
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 12:47:39 -0500

    P04 is started

---
+++ src/test/java/com/cydeo/day02/P04_HamCrestSpartan.java
@@ -0,0 +1,19 @@
+package com.cydeo.day02;
+
+public class P04_HamCrestSpartan {
+    /**
+     * - Send a request to GET /spartans/search
+     * - Query Parameters values are
+     *     - gender —> Female
+     *     - nameContains —> ea
+     * - Log Everything
+     * - Verify followings
+     *       - Status Code is 200
+     *       - ContentType is application/json
+     *       - Total Element 6
+     *       - jsonArray size hasSize 6
+     *       - All Names hasItem "Sean"
+     *       - Every gender is Male
+     */
+
+}
