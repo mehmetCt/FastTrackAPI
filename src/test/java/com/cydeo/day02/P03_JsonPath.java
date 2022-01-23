583d49d7c179386914da5ae71bd8ee81b37c9729
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 11:51:25 -0500

    P03 is started

---
+++ src/test/java/com/cydeo/day02/P03_JsonPath.java
@@ -0,0 +1,24 @@
+package com.cydeo.day02;
+
+import org.junit.jupiter.api.Test;
+
+public class P03_JsonPath {
+
+    @Test
+    public void getLocations() {
+            /*
+    Given
+             accept type is application/json
+     When
+             user sends get request to /locaitons
+     Then
+             response status code must be 200
+             content type equals to application/json
+             get the second city with JsonPath
+             get the last city with JsonPath
+             get all country ids
+             get all city where their country id is UK
+
+  */
+    }
+}
