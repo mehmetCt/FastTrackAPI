e6b7ceaeec5a1acdb43681247ad2adf57bc35f24
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 12:07:12 -0500

    P03 is started

--- src/test/java/com/cydeo/day02/P03_JsonPath.java
+++ src/test/java/com/cydeo/day02/P03_JsonPath.java
@@ -1,8 +1,17 @@
 package com.cydeo.day02;
 
+import com.cydeo.utility.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class P03_JsonPath {
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+public class P03_JsonPath extends HrTestBase {
 
     @Test
     public void getLocations() {
@@ -20,5 +29,36 @@ public class P03_JsonPath {
              get all city where their country id is UK
 
   */
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .log().uri().
+                when()
+                .get("/locations").prettyPeek();
+
+
+        JsonPath jp = response.jsonPath();
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        //second city name
+        System.out.println("jp.getString(\"items[1].city\") = " + jp.getString("items[1].city"));
+
+
+        //last city name
+        System.out.println("jp.getString(\"items[-1].city\") = " + jp.getString("items[-1].city"));
+
+
+        //get all country ids
+        List<String> allCountryIDs = jp.getList("items.country_id");
+        System.out.println("allCountryIDs = " + allCountryIDs);
+
+
+        // get all city where their country id is UK
+        List<String> allCityNamesUK = jp.getList("items.findAll {it.country_id=='UK'}.city");
+        System.out.println("allCityNamesUK = " + allCityNamesUK);
+
+
+
     }
 }
