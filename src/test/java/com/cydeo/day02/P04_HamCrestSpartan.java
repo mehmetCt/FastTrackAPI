564b7322522a8bf2eacabcad009578976fd34509
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 13:02:05 -0500

    P04 is DONE

--- src/test/java/com/cydeo/day02/P04_HamCrestSpartan.java
+++ src/test/java/com/cydeo/day02/P04_HamCrestSpartan.java
@@ -1,6 +1,12 @@
 package com.cydeo.day02;
 
-public class P04_HamCrestSpartan {
+import com.cydeo.utility.SpartanTestbase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P04_HamCrestSpartan extends SpartanTestbase {
     /**
      * - Send a request to GET /spartans/search
      * - Query Parameters values are
@@ -10,10 +16,38 @@ public class P04_HamCrestSpartan {
      * - Verify followings
      *       - Status Code is 200
      *       - ContentType is application/json
-     *       - Total Element 6
-     *       - jsonArray size hasSize 6
-     *       - All Names hasItem "Sean"
-     *       - Every gender is Male
+     *       - Total Element 2
+     *       - jsonArray size hasSize 2
+     *       - All Names hasItem "Meade1"
+     *       - Every gender is Female
      */
 
+    @Test
+    public void searchSpartans() {
+
+        given()
+                .queryParam("nameContains", "ea")
+                .queryParam("gender","Female")
+                .log().uri().
+        when()
+                .get("/spartans/search")  .prettyPeek().
+        then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("totalElement", is(2))
+                .body("content", hasSize(2))
+                .body("content.name",hasItem("Meade1"))
+                .body("content.gender",everyItem(is("Female")))
+        ;
+
+
+
+
+
+
+
+
+
+
+    }
 }
