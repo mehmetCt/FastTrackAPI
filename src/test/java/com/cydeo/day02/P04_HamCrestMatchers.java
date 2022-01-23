c4f29796cb1183e11dea89bbf0da44a89050742e
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 12:35:33 -0500

    P04 is started

---
+++ src/test/java/com/cydeo/day02/P04_HamCrestMatchers.java
@@ -0,0 +1,26 @@
+package com.cydeo.day02;
+
+import org.junit.jupiter.api.Test;
+
+public class P04_HamCrestMatchers {
+
+    /*
+      Given
+               accept type is application/json
+       When
+               user sends get request to /regions
+       Then
+               response status code must be 200
+               verify Date has values
+               first region name is Europe
+               Regions name should be same order as "Europe","Americas","Asia","Middle East and Africa"
+               region ids needs to be 1,2,3,4
+               ...
+               ..
+               .
+    */
+
+    @Test
+   public  void getAllRegions() {
+    }
+}
