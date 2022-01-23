9b17b64e39e195d7321d210297fd35f2b88dc7f4
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 11:09:17 -0500

    Day02 is started

---
+++ src/test/java/com/cydeo/day02/P01_Contains.java
@@ -0,0 +1,16 @@
+package com.cydeo.day02;
+
+public class P01_Contains {
+
+    /*
+       Given
+                accept type is application/json
+        When
+                user sends get request to /regions/2
+        Then
+                response status code must be 200
+                content type equals to application/json
+                response body contains   Americas
+
+     */
+}
