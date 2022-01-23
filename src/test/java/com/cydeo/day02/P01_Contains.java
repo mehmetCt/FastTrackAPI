23d022bd8150baa3352f66d20bd8cb7f7a48aa80
Mehmet <mhmtsytrk@gmail.com>
2022-01-23 11:37:03 -0500

    P01 is completed

--- src/test/java/com/cydeo/day02/P01_Contains.java
+++ src/test/java/com/cydeo/day02/P01_Contains.java
@@ -1,7 +1,15 @@
 package com.cydeo.day02;
 
 import com.cydeo.utility.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.*;
 
 public class P01_Contains extends HrTestBase {
 
@@ -18,6 +26,84 @@ public class P01_Contains extends HrTestBase {
      */
 
     @Test
-    void getOneRegion() {
+    public void getOneRegion() {
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .log().uri()
+                .pathParam("id", 2).
+                when()
+                .get("/regions/{id}").prettyPeek();
+
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(ContentType.JSON.toString(), response.contentType());
+
+
+        //verify
+        Assertions.assertTrue(response.asString().contains("Americas"));
+
+
+
+
     }
+
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    public void getOneRegionValueSource(int id) {
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .log().uri()
+                .pathParam("id", id).
+                when()
+                .get("/regions/{id}").prettyPeek();
+
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(ContentType.JSON.toString(), response.contentType());
+
+
+        //verify
+        Assertions.assertEquals(id, (Integer) response.path("region_id"));
+    }
+
+
+    // numLineToSkip skip the header if it has a value as 2
+    @ParameterizedTest
+    @CsvFileSource(resources = "/regions.csv",numLinesToSkip = 1)
+    public void getOneRegionCsvFileSource(int id,String regionName) {
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .log().uri()
+                .pathParam("id", id).
+                when()
+                .get("/regions/{id}").prettyPeek();
+
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(ContentType.JSON.toString(), response.contentType());
+
+
+        //verify
+        Assertions.assertEquals(id, (Integer) response.path("region_id"));
+        Assertions.assertEquals(regionName, response.path("region_name").toString());
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
