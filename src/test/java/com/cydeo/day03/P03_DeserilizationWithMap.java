32aacb36711b1d44154593c574e521cc25b25832
Mehmet <mhmtsytrk@gmail.com>
2022-01-25 20:45:41 -0500

    Day04 P03 is started

---
+++ src/test/java/com/cydeo/day03/P03_DeserilizationWithMap.java
@@ -0,0 +1,32 @@
+package com.cydeo.day03;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+
+public class P03_DeserilizationWithMap {
+
+    /**
+     * Create a test called getLocation
+     * 1. Send request to GET /locations
+     * 2. log uri to see
+     * 3. Get all Json as a map and print out screen all the things with the help of  map
+     * System.out.println("====== GET FIRST LOCATION  ======");
+     * System.out.println("====== GET FIRST LOCATION LINKS  ======");
+     * System.out.println("====== GET ALL LOCATIONS AS LIST OF MAP======");
+     * System.out.println("====== FIRST LOCATION ======");
+     * System.out.println("====== FIRST LOCATION ID ======");
+     * System.out.println("====== FIRST LOCATION COUNTRY_ID ======");
+     * System.out.println("====== GET FIRST LOCATION FIRST LINK  ====== ");
+     * System.out.println("====== LAST LOCATION ID ======");
+     */
+
+
+
+    @Test
+    public void getAllLocations() {
+
+
+
+    }
+}
